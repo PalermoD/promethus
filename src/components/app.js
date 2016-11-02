ed184bb0d145285d2f26088a14335db7e33f7981
Palermo <palermo@bond.co>
2016-11-02 11:18:29 -0400

    basic stucture for application

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,16 @@
+'use strict'
+
 import React, {Component} from 'react'
+import DocumentTitle from 'react-document-title'
 
 class App extends Component {
 	render(){
 		return(
-			<div>Promethus is born</div>
+			<DocumentTitle title="Soulster | sign up page">
+				<div>
+					<h1>Sign up page</h1>
+				</div>
+			</DocumentTitle>
 		)
 	}
 }
